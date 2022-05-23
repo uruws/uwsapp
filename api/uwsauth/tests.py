3ed137e1a46cab0f82ae24673010cb9bbf02b182
Jeremías <jeremias@talkingpts.org>
2022-05-23 17:11:13 -0300

    uwsauth: app initial commit

---
+++ api/uwsauth/tests.py
@@ -0,0 +1,6 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from django.test import TestCase
+
+# Create your tests here.
---
+++ api/uwsauth/views.py
@@ -0,0 +1,6 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from django.shortcuts import render
+
+# Create your views here.
