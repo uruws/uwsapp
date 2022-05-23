05de90d93f00e876edceaa293099d9dcf6801ca4
Jeremías <jeremias@talkingpts.org>
2022-05-23 17:05:17 -0300

    django app skeleton files

---
+++ _skel/apps.py
@@ -0,0 +1,8 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from django.apps import AppConfig
+
+class AuthConfig(AppConfig):
+	default_auto_field = 'django.db.models.BigAutoField'
+	name = 'CHANGEME'
