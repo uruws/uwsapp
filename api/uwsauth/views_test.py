b89cf323ce8301f016f6c69f8622be24d79a4a9c
Jeremías <jeremias@talkingpts.org>
2022-05-24 00:23:43 -0300

    auth: views test

---
+++ api/uwsauth/views_test.py
@@ -0,0 +1,10 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from django.test import TestCase
+
+class AuthViewsTests(TestCase):
+
+	def test_index(t):
+		resp = t.client.get('/auth/')
+		t.assertEqual(resp.status_code, 200)
