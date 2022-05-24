53b7423a0cd25d835e0b47b0600b33c2ba83cb3e
Jeremías <jeremias@talkingpts.org>
2022-05-23 23:00:27 -0300

    test coverage report

--- docker/devel/run.sh
+++ docker/devel/run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -eu
+install -v -d -m 0750 ${PWD}/tmp
 install -v -d -m 0750 ${PWD}/data
 exec docker run -it --rm --name uwsapp-devel \
 	--hostname devel.uwsapp.local \
 	--read-only \
 	-v ${PWD}:/opt/uws \
+	-v ${PWD}/tmp:/opt/uws/tmp \
 	-v ${PWD}/data:/var/opt/uwsapp \
 	-p 127.0.0.1:3000:3000 \
 	uwsapp/devel
---
+++ test/coverage.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eu
+
+app="${1:?'app?'}"
+omit='/usr/lib/*,_skel/*,./*/manage.py'
+
+rm -f .coverage
+
+python3-coverage erase
+python3-coverage run --omit "${omit}" "./${app}/manage.py" test
+
+install -vd -m 0750 /opt/uws/tmp/htmlcov
+covd="/opt/uws/tmp/htmlcov/${app}"
+rm -rf "${covd}"
+
+python3-coverage report
+python3-coverage html -d "${covd}"
+
+exit 0
