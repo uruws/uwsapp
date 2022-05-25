121feeb88dad2b7ea3d957dc5094f2412f52e97e
Jeremías <jeremias@talkingpts.org>
2022-05-25 00:13:06 -0300

    fix typecheck

--- test/typecheck.sh
+++ test/typecheck.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 set -eu
-exec python3 -m mypy /opt/uwsapp/api
+exec python3 -m mypy \
+	--exclude api/manage.py \
+	--exclude web/manage.py \
+	/opt/uwsapp/core \
+	/opt/uwsapp/api \
+	/opt/uwsapp/web
