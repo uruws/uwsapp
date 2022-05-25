f59dcfbf9ed9260d19b3ff31df9fa5a8e36ce5a9
Jeremías <jeremias@talkingpts.org>
2022-05-24 23:59:27 -0300

    core: config module

---
+++ core/uwsapp/config.py
@@ -0,0 +1,34 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from os         import getenv
+from pathlib    import Path
+from subprocess import getoutput
+
+__unset = '__UNSET__'
+
+__secret_key = getenv('UWSAPP_SECRET', __unset)
+if __secret_key is __unset:
+	__secret_key = getoutput('/usr/bin/pwgen -1snyB 64')
+
+def SECRET_KEY() -> str:
+	return __secret_key
+
+def _getenv(name: str, default) -> str:
+	val = getenv(name, default)
+	if val is __unset:
+		val = str(default)
+	return val
+
+def DEBUG() -> bool:
+	return _getenv('UWSAPP_DEBUG', 'off') == 'on'
+
+def ALLOWED_HOSTS() -> list:
+	if DEBUG(): return []
+	return [_getenv('UWSAPP_HOST', 'localhost')]
+
+def DBDIR() -> Path:
+	return Path(_getenv('UWSAPP_DATADIR', '/var/opt/uwsapp'))
+
+def DBNAME() -> str:
+	return _getenv('UWSAPP_DBNAME',  'api.db')
