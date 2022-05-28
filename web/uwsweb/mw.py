b8bd32e7893418ef910468ab7afe830c45a601d2
Jeremías <jeremias@talkingpts.org>
2022-05-28 02:08:48 -0300

    web: login required

--- web/uwsweb/mw.py
+++ web/uwsweb/mw.py
@@ -1,12 +1,22 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators        import method_decorator
+
 class WebMiddleware:
 
 	def __init__(mw, get_resp):
 		mw.get_resp = get_resp
 
+	@method_decorator(login_required(login_url = '/login'))
+	def __auth(mw, req):
+		return mw.get_resp(req)
+
 	def __call__(mw, req):
-		resp = mw.get_resp(req)
+		if req.path == '/login':
+			resp = mw.get_resp(req)
+		else:
+			resp = mw.__auth(req)
 		resp.headers['Server'] = 'uwsweb'
 		return resp
