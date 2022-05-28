e83a76c4a0bf52282840b648d36bee5c66e4b1e9
Jeremías <jeremias@talkingpts.org>
2022-05-27 23:17:57 -0300

    docker: compose service ports

--- docker/devel/run.sh
+++ docker/devel/run.sh
@@ -8,5 +8,4 @@ exec docker run -it --rm --name uwsapp-devel \
 	-v ${PWD}:/opt/uwsapp \
 	-v ${PWD}/tmp:/opt/uwsapp/tmp \
 	-v ${PWD}/data:/var/opt/uwsapp \
-	-p 127.0.0.1:3000:3000 \
 	uwsapp/devel
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -eu
 app="${1:?'app?'}"
 ./${app}/manage.py migrate
-exec ./${app}/manage.py runserver 0.0.0.0:3000
+exec ./${app}/manage.py runserver 0.0.0.0:${UWSAPP_PORT}
