0554d2675313cf8af2a71854419786a076519900
Jeremías <jeremias@talkingpts.org>
2022-05-28 01:19:25 -0300

    web: check index view content type

--- web/uwsweb/views_test.py
+++ web/uwsweb/views_test.py
@@ -8,5 +8,6 @@ class ApiViewsTests(TestCase):
 	def test_index(t):
 		resp = t.client.get('/')
 		t.assertEqual(resp.status_code, 200)
+		t.assertEqual(resp.headers['content-type'], 'text/html; charset=utf-8')
 		t.assertListEqual(sorted([str(t.name) for t in resp.templates]),
 			['uwsweb/base.html', 'uwsweb/index.html'])
