14c11951fc60a1f5405d5074a90197ce002f39f3
Jeremías <jeremias@talkingpts.org>
2022-05-28 01:16:33 -0300

    web: test index view templates

--- web/uwsweb/views_test.py
+++ web/uwsweb/views_test.py
@@ -8,3 +8,5 @@ class ApiViewsTests(TestCase):
 	def test_index(t):
 		resp = t.client.get('/')
 		t.assertEqual(resp.status_code, 200)
+		t.assertListEqual(sorted([str(t.name) for t in resp.templates]),
+			['uwsweb/base.html', 'uwsweb/index.html'])
