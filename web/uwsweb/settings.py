3ecad7776b88310c4b1e10568e04053cf9368748
Jeremías <jeremias@talkingpts.org>
2022-05-28 00:50:19 -0300

    web: load static

--- web/uwsweb/settings.py
+++ web/uwsweb/settings.py
@@ -77,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'uwsweb.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -121,6 +120,9 @@ USE_TZ    = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+	BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
