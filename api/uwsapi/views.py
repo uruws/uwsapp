70181ef8d49e4d7337916d778668984222efad1e
Jeremías <jeremias@talkingpts.org>
2022-06-03 20:10:41 -0300

    api: debug information

--- api/uwsapi/views.py
+++ api/uwsapi/views.py
@@ -2,6 +2,7 @@
 # See LICENSE file.
 
 from django.http import JsonResponse
+from os          import environ
 
 from uwsapp import config
 
@@ -9,4 +10,11 @@ def index(req):
 	if config.DEBUG(): return _debug(req)
 	resp = JsonResponse(dict())
 	resp.status_code = 404
+	return resp
+
+def _debug(req):
+	d = dict()
+	for k in sorted(environ.keys()):
+		d[k] = environ.get(k)
+	resp = JsonResponse(d)
 	return resp
