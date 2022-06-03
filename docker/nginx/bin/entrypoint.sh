2c0f3a7074ef07a9f4bcd9f210c4474ad35f0317
Jeremías <jeremias@talkingpts.org>
2022-06-02 21:32:25 -0300

    nginx: devel CA files

--- docker/nginx/bin/entrypoint.sh
+++ docker/nginx/bin/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 set -eu
+install -v -m 0644 -o uws -g uws \
+	/usr/local/etc/ca/client/5946eae9-e236-5a12-8b59-0b94b112f6a1.p12 \
+	/opt/uwsapp/tmp/devel-local.p12
 exec /usr/sbin/nginx -g "daemon off;"
