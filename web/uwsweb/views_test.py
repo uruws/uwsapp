f6ced673cb68084db85b0c3bf92a7a97931721cc
Jeremías <jeremias@talkingpts.org>
2022-06-06 21:22:33 -0300

    web: fix nologin test

--- web/uwsweb/views_test.py
+++ web/uwsweb/views_test.py
@@ -5,8 +5,8 @@ from django.test import TestCase
 
 class ApiViewsTests(TestCase):
 
-	def test_index(t):
+	def test_index_nologin(t):
 		resp = t.client.get('/')
-		t.assertEqual(resp.status_code, 200)
+		t.assertEqual(resp.status_code, 302)
 		t.assertEqual(resp.headers['content-type'], 'text/html; charset=utf-8')
-		t.assertEqual(resp.template_name, ['uwsweb/index.html'])
+		t.assertEqual(resp.headers['location'], '/login?next=/')
