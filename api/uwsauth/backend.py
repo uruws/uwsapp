098eb621c6029d7025158c8f75f15e6c5d5fc8dc
Jeremías <jeremias@talkingpts.org>
2022-06-09 20:05:22 -0300

    api: auth backend module funcs

--- api/uwsauth/backend.py
+++ api/uwsauth/backend.py
@@ -21,6 +21,47 @@ def _user_uuid(username: str) -> str:
 def _user_password(pw: str) -> str:
 	return pbkdf2_hmac('sha256', pw.encode(), __salt, 100000).hex()
 
+def _check_password(uid: str, fn: Path, password: str) -> bool:
+	pw = fn.read_text().strip()
+	upw = _user_password(password)
+	if pw == upw:
+		return True
+	log.debug(uid, 'auth password:', pw)
+	log.debug(uid, 'user password:', upw)
+	return False
+
+def _load_user(uid: str, fn: Path, username: str):
+	u = None
+	with open(fn, 'r') as fh:
+		u = json.load(fh)
+	try:
+		if u['username'] != username:
+			log.error('%s: auth info mismatch' % uid)
+			return None
+	except KeyError:
+		log.error('%s: invalid auth info' % uid)
+		return None
+	log.print('auth:', uid)
+	return User(username = username)
+
+def _check_credentials(uid: str, username: str, password: str):
+	fn = Path('/run/uwscli/auth/%s/meta.json' % uid)
+	pwfn = Path('/run/uwscli/auth/%s/password' % uid)
+	if fn.is_file() and not fn.is_symlink():
+		if pwfn.is_file() and not pwfn.is_symlink():
+			try:
+				if _check_password(uid, pwfn, password):
+					return _load_user(uid, fn, username)
+				else:
+					log.error('%s: invalid password' % uid)
+			except Exception as err:
+				log.error('check user credentials:', err)
+		else:
+			log.error('%s: file not found or is a symlink' % pwfn)
+	else:
+		log.error('%s: file not found or is a symlink' % fn)
+	return None
+
 class AuthBackend(BaseBackend):
 
 	def authenticate(b, request, username: str = None, password: str = None):
@@ -33,45 +74,4 @@ class AuthBackend(BaseBackend):
 			return None
 		uid = _user_uuid(username)
 		log.debug('uid:', uid)
-		return b.__check_credentials(uid, username, password)
-
-	def __check_credentials(b, uid: str, username: str, password: str):
-		fn = Path('/run/uwscli/auth/%s/meta.json' % uid)
-		pwfn = Path('/run/uwscli/auth/%s/password' % uid)
-		if fn.is_file() and not fn.is_symlink():
-			if pwfn.is_file() and not pwfn.is_symlink():
-				try:
-					if b.__check_password(uid, pwfn, password):
-						return b.__load_user(uid, fn, username)
-					else:
-						log.error('%s: invalid password' % uid)
-				except Exception as err:
-					log.error('check user credentials:', err)
-			else:
-				log.error('%s: file not found or is a symlink' % pwfn)
-		else:
-			log.error('%s: file not found or is a symlink' % fn)
-		return None
-
-	def __check_password(b, uid: str, fn: Path, password: str) -> bool:
-		pw = fn.read_text().strip()
-		upw = _user_password(password)
-		if pw == upw:
-			return True
-		log.debug(uid, 'auth password:', pw)
-		log.debug(uid, 'user password:', upw)
-		return False
-
-	def __load_user(b, uid: str, fn: Path, username: str):
-		u = None
-		with open(fn, 'r') as fh:
-			u = json.load(fh)
-		try:
-			if u['username'] != username:
-				log.error('%s: auth info mismatch' % uid)
-				return None
-		except KeyError:
-			log.error('%s: invalid auth info' % uid)
-			return None
-		log.print('auth:', uid)
-		return User(username = username)
+		return _check_credentials(uid, username, password)
