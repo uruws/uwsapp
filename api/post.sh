01142d5285cecf1b9c28b4e4e10344a12fd7756c
Jeremías <jeremias@talkingpts.org>
2022-06-10 01:11:15 -0300

    api: verbose devel curl scripts

--- api/post.sh
+++ api/post.sh
@@ -2,6 +2,6 @@
 set -eu
 session=${1:?'session?'}
 url=${2:?'url?'}
-curl -d "session=${session}" "http://localhost:3000${url}"
+curl -v -d "session=${session}" "http://localhost:3000${url}"
 echo
 exit 0
