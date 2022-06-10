36c12548b77893f3c7b10ffee82488a809421a70
Jeremías <jeremias@talkingpts.org>
2022-06-10 19:53:49 -0300

    nginx: export devel cert files

--- docker/nginx/bin/entrypoint.sh
+++ docker/nginx/bin/entrypoint.sh
@@ -5,6 +5,14 @@ install -v -m 0644 -o uws -g uws \
 	/usr/local/etc/ca/client/5946eae9-e236-5a12-8b59-0b94b112f6a1.p12 \
 	/opt/uwsapp/tmp/devel-local.p12
 
+install -v -m 0644 -o uws -g uws \
+	/usr/local/etc/ca/client/5946eae9-e236-5a12-8b59-0b94b112f6a1.pem \
+	/opt/uwsapp/tmp/devel-local.pem
+
+install -v -m 0644 -o uws -g uws \
+	/usr/local/etc/ca/client/5946eae9-e236-5a12-8b59-0b94b112f6a1-key.pem \
+	/opt/uwsapp/tmp/devel-local-key.pem
+
 /usr/sbin/nginx -t
 
 exec /usr/sbin/nginx -g "daemon off;"
