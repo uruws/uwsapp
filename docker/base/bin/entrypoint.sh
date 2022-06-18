76d119a9add462332ce6bf6691b832697a5c3e79
Jeremías <jeremias@talkingpts.org>
2022-06-18 03:04:15 -0300

    docker: fix entrypoint.sh

--- docker/base/bin/entrypoint.sh
+++ docker/base/bin/entrypoint.sh
@@ -3,6 +3,7 @@
 # https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/uwsgi/
 #
 set -eu
+appname="uws${UWSAPP_NAME}"
 exec uwsgi \
 	--master \
 	--no-orphans \
@@ -14,14 +15,13 @@ exec uwsgi \
 	--max-apps				1 \
 	--reload-mercy			60 \
 	--worker-reload-mercy	30 \
-	--harakiri				20 \
 	--max-requests			5000 \
 	--min-worker-lifetime	60 \
 	--max-worker-lifetime	28800 \
 	--evil-reload-on-rss	1024 \
-	--module				uwsapp.wsgi:application \
-	--env					DJANGO_SETTINGS_MODULE=uwsapp.settings \
+	--module				"${appname}.wsgi:application" \
+	--env					"DJANGO_SETTINGS_MODULE=${appname}.settings" \
 	--env					LANG=es_US.UTF-8 \
 	--workers				"${UWSAPP_WORKERS}" \
 	--http11-socket			"0.0.0.0:${UWSAPP_PORT}" \
-	--chdir					"${UWSAPP_HOME}"
+	--chdir					"${UWSAPP_HOME}/${UWSAPP_NAME}"
