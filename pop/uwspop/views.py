165a83b4881ce2c2655b1d2a15b640eaa9c7972e
Jeremías <jeremias@talkingpts.org>
2022-06-20 20:27:58 -0300

    pop: load config from OS env

--- pop/uwspop/views.py
+++ pop/uwspop/views.py
@@ -3,9 +3,32 @@
 
 from django.http import HttpRequest
 from django.http import JsonResponse
-from http        import HTTPStatus
+
+from http   import HTTPStatus
+from os     import getenv
+from poplib import POP3_SSL
 
 from uwsapp import log
+
+_hostname: str = 'NOHOSTNAME'
+_port:     int = -1
+_timeout:  int = -1
+
+def _loadenv():
+	global _hostname
+	global _port
+	global _timeout
+	_hostname = getenv('UWSPOP_HOSTNAME', 'localhost')
+	try:
+		_port = int(getenv('UWSPOP_PORT', 995))
+		_timeout = int(getenv('UWSPOP_TIMEOUT', 15))
+	except ValueError as err:
+		raise RuntimeError(f"UWSPOP_PORT: invalid setting:", err)
+
+_loadenv()
+
+def _connect(username: str, password: str) -> POP3_SSL:
+	return POP3_SSL(_hostname, port = _port, timeout = _timeout)
 
 def index(req: HttpRequest) -> JsonResponse:
 	log.debug('req:', req)
