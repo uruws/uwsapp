34c94fe40fe4b7e2651b8c30b6f42a802591ea55
Jeremías <jeremias@talkingpts.org>
2022-06-20 19:40:45 -0300

    fix skel and pop APPNAME config

--- pop/uwspop/settings.py
+++ pop/uwspop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 from uwsapp  import config
 
-config.APPNAME = 'web'
+config.APPNAME = 'pop'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
