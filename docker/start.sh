7d895a78383b1e1335e598ca9454788368e237ef
Jeremías <jeremias@talkingpts.org>
2022-07-08 19:41:08 -0300

    deploy: docker start do not build

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -eu
-
 appenv=${1:?'app env?'}
-
 appdir="/srv/uwsapp/${appenv}"
-
-install -v -d -m 0750 "${appdir}/data"
-install -v -d -m 0750 "${appdir}/run"
-install -v -d -m 0750 "${appdir}/run/uwscli"
-install -v -d -m 0750 "${appdir}/run/uwsapp"
-
 cd "${appdir}"
-exec docker-compose up --build
+exec docker-compose up
