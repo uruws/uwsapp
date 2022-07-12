b846f1dcbd0acc1deab0ffe6d8e113369c624dad
Jeremías <jeremias@talkingpts.org>
2022-07-12 00:07:08 -0300

    help: fix mw setting

--- help/uwshelp/settings.py
+++ help/uwshelp/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	'uwshelp.mw.HelpMiddleware',
 ]
 
 ROOT_URLCONF = 'uwshelp.urls'
