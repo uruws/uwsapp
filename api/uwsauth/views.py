067c8a31af91faaa255d002995c9cba75fdbd691
Jeremías <jeremias@talkingpts.org>
2022-07-13 18:12:24 -0300

    api: login send user info

--- api/uwsauth/views.py
+++ api/uwsauth/views.py
@@ -1,14 +1,29 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
+import json
+
 from django.contrib.auth        import authenticate
 from django.contrib.auth.models import User
 from django.http                import JsonResponse
 
 from django.contrib.sessions.backends.db import SessionStore
 
-from http import HTTPStatus
-from time import time
+from http    import HTTPStatus
+from pathlib import Path
+from time    import time
+
+from uwsapp import log
+from uwsapp import user
+
+def _load_user(username: str) -> dict[str, str]:
+	uid = user.uuid(username)
+	log.debug('load_user:', uid, username)
+	u = {}
+	with Path('/run/uwscli/auth/%s/meta.json' % uid).open() as fh:
+		u = json.load(fh)
+	u['uid'] = uid
+	return u
 
 def login(req):
 	if req.method == 'POST':
@@ -24,13 +39,15 @@ def login(req):
 			resp.status_code = HTTPStatus.UNAUTHORIZED
 			return resp
 		else:
+			u = _load_user(username)
 			sess = SessionStore()
 			sess.create()
 			sess['username'] = username
 			sess['last_seen'] = time()
 			sess.set_expiry(3600) # after one hour of inactivity
 			sess.save()
-			return JsonResponse({'session': sess.session_key})
+			u['session'] = sess.session_key
+			return JsonResponse(u)
 	resp = JsonResponse({})
 	resp.status_code = HTTPStatus.BAD_REQUEST
 	return resp
