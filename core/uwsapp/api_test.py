a2fb353812ac8827097f40c459b16a43365d374e
Jeremías <jeremias@talkingpts.org>
2022-07-12 22:36:12 -0300

    core: api client module

---
+++ core/uwsapp/api_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+import unittest
+from unittest.mock import MagicMock
+
+from uwsapp import api
+
+class Test(unittest.TestCase):
+
+	def test_fake(t):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
