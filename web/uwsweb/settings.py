c7372dd1d92d3656404eb4169f87bf450bd850a1
Jeremías <jeremias@talkingpts.org>
2022-07-13 12:21:32 -0300

    web: relocate template files

--- web/uwsweb/settings.py
+++ web/uwsweb/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
 		'DIRS': [
 			BASE_DIR.parent / 'core' / 'templates',
+			BASE_DIR.parent / 'web' / 'templates',
 		],
 		'APP_DIRS': True,
 		'OPTIONS': {
