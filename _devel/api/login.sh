4f86e2ca3eaddc12f7c8ef1769a090c64d1e0c2a
Jeremías <jeremias@talkingpts.org>
2022-07-20 19:34:47 -0300

    devel: api password

--- _devel/api/login.sh
+++ _devel/api/login.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-curl -v -d 'username=uwsdev@uwsapp.local&password=123456' \
+curl -v -d 'username=uwsdev@uwsapp.local&password=supersecret' \
 	http://localhost:3000/auth/login
 echo
 exit 0
