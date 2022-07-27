2f56eae4bebb613f73c83519602248be87e48bed
Jeremías <jeremias@talkingpts.org>
2022-07-27 18:51:28 -0300

    api: catch uwslogs views exceptions

--- api/uwslogs/views.py
+++ api/uwslogs/views.py
@@ -5,21 +5,31 @@ from django.http import JsonResponse
 
 from uwsapi.views import ApiView
 
+from uwsapp import log
+
 from . import nqlog
 from . import syslog
 
 class Index(ApiView):
 
 	def post(v, req, name = '') -> JsonResponse:
-		if name == 'app-ctl':
-			return v.uwsapi_resp(syslog.app_ctl().all())
-		if name == 'uwsq':
-			return v.uwsapi_resp(syslog.uwsq().all())
+		try:
+			if name == 'app-ctl':
+				return v.uwsapi_resp(syslog.app_ctl().all())
+			if name == 'uwsq':
+				return v.uwsapi_resp(syslog.uwsq().all())
+		except Exception as err:
+			log.error(err)
+			return v.uwsapi_internal_error()
 		return v.uwsapi_bad_request()
 
 class NQ(ApiView):
 
 	def post(v, req, jobid = '') -> JsonResponse:
-		if name == 'index':
-			return v.uwsapi_resp(nqlog.jobs().all())
+		try:
+			if name == 'index':
+				return v.uwsapi_resp(nqlog.jobs().all())
+		except Exception as err:
+			log.error(err)
+			return v.uwsapi_internal_error()
 		return v.uwsapi_bad_request()
