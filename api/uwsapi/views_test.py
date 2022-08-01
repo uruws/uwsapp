f7f9833d704aaef7c8148b733d0e7e65b7448834
Jeremías <jeremias@talkingpts.org>
2022-08-01 20:40:11 -0300

    api: mock login create and delete user

--- api/uwsapi/views_test.py
+++ api/uwsapi/views_test.py
@@ -9,34 +9,38 @@ from time import time
 from django.contrib.auth.models          import User
 from django.contrib.sessions.backends.db import SessionStore
 
+from uwsapp import config
+from uwsapp import log
+
 class ApiMock(object):
 	sess     = None
 	sess_key = None
 	username = 'uwstest@localhost'
+	user     = None
 
 	def mock_user(m):
 		user = User(username = 'uwstest', email = m.username)
 		user.save()
 		return user
 
-	def mock_login_setup(m, session_key = None):
-		m.sess = SessionStore(session_key = session_key)
+	def mock_login_setup(m):
+		m.user = m.mock_user()
+		m.sess = SessionStore()
 		m.sess.create()
 		m.sess['last_seen'] = time()
 		m.sess['username'] = m.username
 		m.sess.save()
 		m.sess_key = m.sess.session_key
 
-	def mock_login_teardown(m, session_key = None):
+	def mock_login_teardown(m):
 		m.sess.delete()
+		if m.user.pk is not None:
+			m.user.delete()
 
 class ApiViewsTests(TestCase):
 	api = ApiMock()
-	user = None
 
 	def setUp(t):
-		if t.user is None:
-			t.user = t.api.mock_user()
 		t.api.mock_login_setup()
 
 	def tearDown(t):
