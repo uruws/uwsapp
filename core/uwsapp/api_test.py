f25115e744b6dcf27e7326c80a4b4bc0f6e07bf8
Jeremías <jeremias@talkingpts.org>
2022-08-01 17:41:13 -0300

    core: api client tests

--- core/uwsapp/api_test.py
+++ core/uwsapp/api_test.py
@@ -6,6 +6,8 @@
 import unittest
 from unittest.mock import MagicMock
 
+from contextlib import contextmanager
+
 from uwsapp import api
 
 bup_urlopen = api.urlopen
@@ -19,8 +21,24 @@ class Test(unittest.TestCase):
 	def tearDown(t):
 		api.urlopen = bup_urlopen
 
+	@contextmanager
+	def mock_req(t):
+		bup = t.cli._req
+		try:
+			t.cli._req = MagicMock(return_value = None)
+			yield
+		finally:
+			t.cli._req = bup
+
 	def test_POST(t):
 		t.cli.POST('/testing', {'test': 'ing'})
+		api.urlopen.assert_called_once()
+
+	def test_POST_req(t):
+		with t.mock_req():
+			t.cli.POST('/testing', {'test': 'ing'})
+			api.urlopen.assert_called_once()
+			t.cli._req.assert_called_once_with('/testing', {'test': 'ing'})
 
 if __name__ == '__main__':
 	unittest.main()
