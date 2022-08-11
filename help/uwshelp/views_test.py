419a6c298f135b0c3bb6c4a4e1908559ad177edc
Jeremías <jeremias@talkingpts.org>
2022-08-11 17:26:11 -0300

    help: views test

---
+++ help/uwshelp/views_test.py
@@ -0,0 +1,35 @@
+# Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+# See LICENSE file.
+
+from django.test import TestCase
+from http        import HTTPStatus
+
+from uwsapp import config
+
+class HelpViewsTests(TestCase):
+
+	def test_index(t):
+		resp = t.client.get('/')
+		t.assertEqual(resp.status_code, HTTPStatus.OK)
+		t.assertEqual(resp.headers['content-type'], 'text/html; charset=utf-8')
+		t.assertEqual(resp.template_name[0], 'uwshelp/index.html')
+		t.assertEqual(resp.context_data['title'], 'index')
+		t.assertTrue(len(resp.context_data['api_docs']) > 0)
+		t.assertTrue(len(resp.context_data['web_docs']) > 0)
+
+	def test_help(t):
+		resp = t.client.get('/web/api')
+		t.assertEqual(resp.status_code, HTTPStatus.OK)
+		t.assertEqual(resp.headers['content-type'], 'text/html; charset=utf-8')
+		t.assertEqual(resp.template_name[0], 'uwshelp/doc.html')
+		t.assertEqual(resp.context_data['title'], '/web/api')
+		t.assertTrue(len(resp.context_data['doc']) > 0)
+
+	def test_help_error(t):
+		resp = t.client.get('/notfound')
+		t.assertEqual(resp.status_code, HTTPStatus.OK)
+		t.assertEqual(resp.headers['content-type'], 'text/html; charset=utf-8')
+		t.assertEqual(resp.template_name[0], 'uwshelp/doc.html')
+		t.assertEqual(resp.context_data['title'], '/notfound')
+		t.assertEqual(resp.context_data['doc'],
+			"""<pre class="w3-container w3-red">ERROR: [Errno 2] No such file or directory: '/opt/uwsapp/help/docs/notfound.md'</pre>""")
