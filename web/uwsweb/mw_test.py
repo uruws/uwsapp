32fb7163c3392889afa746b471c4fc322a8d5482
Jeremías <jeremias@talkingpts.org>
2022-08-11 18:46:11 -0300

    web: test middleware auth skip

--- web/uwsweb/mw_test.py
+++ web/uwsweb/mw_test.py
@@ -11,11 +11,23 @@ from uwsapp import config
 class HelpViewsTests(WebViewTestCase):
 
 	def test_auth(t):
-		resp = t.client.get('/api')
+		resp = t.client.get('/not.found')
 		t.assertEqual(resp.status_code, HTTPStatus.FOUND)
-		t.assertEqual(resp.headers['location'], '/auth/login?next=/api')
+		t.assertEqual(resp.headers['location'], '/auth/login?next=/not.found')
 
 	def test_auth_login(t):
 		with t.uwsweb_user() as u:
 			resp = t.client.get('/api')
 			t.assertEqual(resp.status_code, HTTPStatus.OK)
+
+	def test_auth_skip_login(t):
+		resp = t.client.get('/auth/login')
+		t.assertEqual(resp.status_code, HTTPStatus.OK)
+
+	def test_auth_skip_static(t):
+		resp = t.client.get('/static/not.found')
+		t.assertEqual(resp.status_code, HTTPStatus.NOT_FOUND)
+
+	def test_auth_skip_admin(t):
+		resp = t.client.get('/admin/login/')
+		t.assertEqual(resp.status_code, HTTPStatus.OK)
