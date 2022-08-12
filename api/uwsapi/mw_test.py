37f4e3c53f8046716b157e504576ea4c963d1a61
Jeremías <jeremias@talkingpts.org>
2022-08-12 16:18:32 -0300

    api: test mw empty session

--- api/uwsapi/mw_test.py
+++ api/uwsapi/mw_test.py
@@ -1,9 +1,25 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
+from contextlib    import contextmanager
+from unittest.mock import MagicMock
+
 from uwsapi.views_test import ApiViewTestCase
 
 from http import HTTPStatus
+
+from uwsapi import mw
+
+@contextmanager
+def mock_session(is_empty = False):
+	bup = mw.SessionStore
+	try:
+		sess = MagicMock()
+		sess.is_empty = MagicMock(return_value = is_empty)
+		mw.SessionStore = MagicMock(return_value = sess)
+		yield
+	finally:
+		mw.SessionStore = bup
 
 class ApiMiddlewareTest(ApiViewTestCase):
 
@@ -31,3 +47,8 @@ class ApiMiddlewareTest(ApiViewTestCase):
 	def test_session_not_found(t):
 		resp = t.client.post('/', {'session': 'abcdef123456'})
 		t.assertEqual(resp.status_code, HTTPStatus.UNAUTHORIZED)
+
+	def test_session_empty(t):
+		with mock_session(is_empty = True):
+			resp = t.uwsapi_post('/ping', {})
+			t.assertEqual(resp.status_code, HTTPStatus.UNAUTHORIZED)
