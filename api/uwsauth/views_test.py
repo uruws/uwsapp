7eb99f93bfad0d715df6bc44223967323c056fcc
Jeremías <jeremias@talkingpts.org>
2022-08-12 17:13:55 -0300

    api: test login view

--- api/uwsauth/views_test.py
+++ api/uwsauth/views_test.py
@@ -1,14 +1,24 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
-from django.test import TestCase
-
 from http import HTTPStatus
 
-class AuthViewsTests(TestCase):
+from uwsapi.views_test import ApiViewTestCase
+
+class AuthViewTest(ApiViewTestCase):
 
 	def test_login(t):
+		resp = t.client.post('/auth/login', {
+			'username': 'uwstest@localhost', 'password': 'supersecret',
+		})
+		t.assertEqual(resp.status_code, HTTPStatus.OK)
+		t.assertEqual(resp.json()['name'], 'uwstest')
+
+	def test_login_unauth(t):
+		resp = t.client.post('/auth/login', {})
+		t.assertEqual(resp.status_code, HTTPStatus.UNAUTHORIZED)
+
+	def test_login_bad_request(t):
 		resp = t.client.get('/auth/login')
 		t.assertEqual(resp.status_code, HTTPStatus.BAD_REQUEST)
-		respdata = resp.json()
-		t.assertEqual(len(respdata), 0)
+		t.assertDictEqual(resp.json(), {})
