0533399dfd29649b4722aa892d586aa14e34dfaf
Jeremías <jeremias@talkingpts.org>
2022-08-12 14:20:32 -0300

    api: test backend get user

--- api/uwsauth/backend_test.py
+++ api/uwsauth/backend_test.py
@@ -3,7 +3,11 @@
 
 from http import HTTPStatus
 
+from django.contrib.auth.models import User
+
 from uwsapi.views_test import ApiViewTestCase
+
+from uwsauth import backend
 
 class AuthBackendTest(ApiViewTestCase):
 
@@ -18,3 +22,26 @@ class AuthBackendTest(ApiViewTestCase):
 	def test_invalid_password(t):
 		t.assertFalse(t.client.login(username = 'uwsdev@uwsapp.local',
 			password = 'invalid'))
+
+	def test_auth_no_data(t):
+		b = backend.AuthBackend()
+		t.assertIsNone(b.authenticate(None))
+
+	def test_auth_empty_data(t):
+		b = backend.AuthBackend()
+		t.assertIsNone(b.authenticate(None, username = '', password = ''))
+
+	def test_get_user(t):
+		b = backend.AuthBackend()
+		t.assertIsNotNone(b.get_user(1))
+
+	def test_get_user_not_found(t):
+		b = backend.AuthBackend()
+		t.assertIsNone(b.get_user(999))
+
+	def test_get_user_inactive(t):
+		u = User.objects.get(pk = 1)
+		u.is_active = False
+		u.save()
+		b = backend.AuthBackend()
+		t.assertIsNone(b.get_user(1))
