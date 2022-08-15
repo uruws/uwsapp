5ee7cf3f8c635cb7499bff61a6a2ab04e637d344
Jeremías <jeremias@talkingpts.org>
2022-08-15 14:27:21 -0300

    api: test nqlog run errors

--- api/uwslogs/nqlog_test.py
+++ api/uwslogs/nqlog_test.py
@@ -1,13 +1,23 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
+from contextlib    import contextmanager
 from unittest.mock import MagicMock
 
 from http import HTTPStatus
 
 from uwsapi.views_test import ApiViewTestCase
 
-from uwslogs import views
+from uwslogs import nqlog
+
+@contextmanager
+def mock_run(status = 0, output = 'mock_output'):
+	bup = nqlog._run
+	try:
+		nqlog._run = MagicMock(return_value = (status, output))
+		yield
+	finally:
+		nqlog._run = bup
 
 class NqlogTest(ApiViewTestCase):
 
@@ -15,3 +25,9 @@ class NqlogTest(ApiViewTestCase):
 		resp = t.uwsapi_post('/logs/nq/index', {})
 		t.assertEqual(resp.status_code, HTTPStatus.OK)
 		t.assertIsInstance(resp.json(), dict)
+
+	def test_run_error(t):
+		with mock_run(status = 99):
+			t.assertTrue(nqlog._jobfail('/run/uwscli/nq/,1823af52ff8.17752'))
+		with mock_run(status = 99):
+			t.assertIsNotNone(nqlog.jobs())
