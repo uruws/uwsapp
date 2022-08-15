74bf846c8991a13fa7fce17f651d769b6a222bb4
Jeremías <jeremias@talkingpts.org>
2022-08-15 15:30:03 -0300

    docker: fix check script

--- docker/devel/check.sh
+++ docker/devel/check.sh
@@ -3,13 +3,19 @@ set -eu
 
 install -v -d -m 0750 ${PWD}/tmp
 install -v -d -m 0750 ${PWD}/data
+install -v -d -m 0750 ${PWD}/run
+install -v -d -m 0750 ${PWD}/run/uwsapp
+
+echo 'supersecret' >${PWD}/run/uwsapp/api_keypass
 
 exec docker run --rm --name uwsapp-check \
 	--hostname check.uwsapp.local \
 	--read-only \
+	-v ${PWD}/docker/devel/run/uwscli:/run/uwscli:ro \
 	-v ${PWD}:/opt/uwsapp \
 	-v ${PWD}/tmp:/opt/uwsapp/tmp \
 	-v ${PWD}/data:/var/opt/uwsapp \
+	-v ${PWD}/run/uwsapp:/run/uwsapp \
 	--workdir /opt/uwsapp \
 	--entrypoint /opt/uwsapp/test/all.sh \
 	uwsapp/devel
