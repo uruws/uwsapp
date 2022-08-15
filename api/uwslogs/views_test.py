16824991fc5fcc53063289f96db3e278c1318eab
Jeremías <jeremias@talkingpts.org>
2022-08-15 12:58:17 -0300

    api: test uwslogs index views

--- api/uwslogs/views_test.py
+++ api/uwslogs/views_test.py
@@ -7,7 +7,7 @@ from uwsapi.views_test import ApiViewTestCase
 
 class LogsViewsTest(ApiViewTestCase):
 
-	def test_index(t):
+	def test_index_uwsq(t):
 		resp = t.uwsapi_post('/logs/uwsq', {})
 		t.assertEqual(resp.status_code, HTTPStatus.OK)
 		data = resp.json()
@@ -18,5 +18,19 @@ class LogsViewsTest(ApiViewTestCase):
 			'source': 'uwsq',
 			'timestamp': 'Fri, 15 Jul 2022 16:45:03 +0000',
 			'user': 'uwscli',
+			'warning': False,
+		})
+
+	def test_index_appctl(t):
+		resp = t.uwsapi_post('/logs/app-ctl', {})
+		t.assertEqual(resp.status_code, HTTPStatus.OK)
+		data = resp.json()
+		t.assertListEqual(list(data.keys()), [str(i) for i in range(111)])
+		t.assertDictEqual(data['0'], {
+			'error': False,
+			'message': 'apptest-east meteor/worker deploy 2.75.0-bp35',
+			'source': 'app-ctl',
+			'timestamp': 'Fri, 15 Jul 2022 17:17:54 +0000',
+			'user': 'uws',
 			'warning': False,
 		})
