676d90e046836adfea81689719dd5cb0eb64aae6
Jeremías <jeremias@talkingpts.org>
2022-08-15 13:05:10 -0300

    api: test uwslogs index errors

--- api/uwslogs/views_test.py
+++ api/uwslogs/views_test.py
@@ -1,9 +1,13 @@
 # Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 # See LICENSE file.
 
+from unittest.mock import MagicMock
+
 from http import HTTPStatus
 
 from uwsapi.views_test import ApiViewTestCase
+
+from uwslogs import views
 
 class LogsViewsTest(ApiViewTestCase):
 
@@ -34,3 +38,23 @@ class LogsViewsTest(ApiViewTestCase):
 			'user': 'uws',
 			'warning': False,
 		})
+
+	def test_index_bad_request(t):
+		resp = t.uwsapi_post('/logs/invalid', {})
+		t.assertEqual(resp.status_code, HTTPStatus.BAD_REQUEST)
+		t.assertDictEqual(resp.json(), {})
+
+	def test_index_error(t):
+		def _err(*args, **kwargs):
+			raise Exception('mock_index_error')
+		bup = views.syslog
+		try:
+			# mock
+			views.syslog = MagicMock()
+			views.syslog.app_ctl = MagicMock(side_effect = _err)
+			# test
+			resp = t.uwsapi_post('/logs/uwsq', {})
+			t.assertEqual(resp.status_code, HTTPStatus.INTERNAL_SERVER_ERROR)
+			t.assertDictEqual(resp.json(), {})
+		finally:
+			views.syslog = bup
