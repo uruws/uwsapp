4a3a5bf207fd7de5a3b425097fc60fc384358ff3
Jeremías <jeremias@talkingpts.org>
2022-09-14 14:55:08 -0300

    api: not found response

--- api/uwsapi/views.py
+++ api/uwsapi/views.py
@@ -46,6 +46,11 @@ class ApiView(View):
 		resp.status_code = status
 		return resp
 
+	def uwsapi_not_found(v, data = {}) -> JsonResponse:
+		resp = JsonResponse(data)
+		resp.status_code = HTTPStatus.NOT_FOUND
+		return resp
+
 	def uwsapi_bad_request(v, data = {}) -> JsonResponse:
 		resp = JsonResponse(data)
 		resp.status_code = HTTPStatus.BAD_REQUEST
