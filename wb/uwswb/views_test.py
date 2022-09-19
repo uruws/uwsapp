1d9ae5721b6b8d29f82c7015cabf7cdc6cd885b3
Jeremías <jeremias@talkingpts.org>
2022-09-19 14:23:51 -0300

    uwswb: whistle blower project

--- wb/uwswb/views_test.py
+++ wb/uwswb/views_test.py
@@ -5,11 +5,11 @@ from django.test import TestCase
 
 from uwsapp import config
 
-class SkelViewsTests(TestCase):
+class WBViewsTests(TestCase):
 
 	def test_index(t):
 		resp = t.client.get('/')
 		t.assertEqual(resp.status_code, HTTPStatus.OK)
 		t.assertEqual(resp.headers['content-type'], 'text/html; charset=utf-8')
-		t.assertEqual(resp.template_name[0], 'uwsSKEL/index.html')
+		t.assertEqual(resp.template_name[0], 'uwswb/index.html')
 		t.assertEqual(resp.context_data['title'], 'index')
--- wb/uwswb/wsgi.py
+++ wb/uwswb/wsgi.py
@@ -14,6 +14,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uwsSKEL.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uwswb.settings')
 
 application = get_wsgi_application()
