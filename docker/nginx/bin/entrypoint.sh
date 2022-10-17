b7738ab7314f71d27591332136f6916b77153c6e
Jeremías <jeremias@talkingpts.org>
2022-10-17 16:47:59 -0300

    nginx: send logs to stderr

--- docker/nginx/bin/entrypoint.sh
+++ docker/nginx/bin/entrypoint.sh
@@ -13,6 +13,11 @@ install -v -m 0644 -o uws -g uws \
 	/usr/local/etc/ca/client/5946eae9-e236-5a12-8b59-0b94b112f6a1-key.pem \
 	/opt/uwsapp/tmp/devel-local-key.pem
 
+rm -vf /var/log/nginx/access.log
+ln -svf /dev/stderr /var/log/nginx/access.log
+rm -vf /var/log/nginx/error.log
+ln -svf /dev/stderr /var/log/nginx/error.log
+
 /usr/sbin/nginx -t
 
 exec /usr/sbin/nginx -g "daemon off;"
